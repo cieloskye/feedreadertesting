20c01cee951165359a0a91936ed32f9582a1ca13
cieloskye <skyeldemoya@gmail.com>
2019-01-28 22:54:28 -0800

    Updated comments.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,39 +1,26 @@
 // feedreader.js
+    /*  This is the spec file that Jasmine will read and contains 
+        all of the tests that will be run against your application.
+    "Jasmine is a behavior-driven development framework for testing 
+    JavaScript code. It does not depend on any other JavaScript 
+    frameworks. It does not require a DOM."" Source: jasmine.github.io
+    */
+
 'use strict';
 
-/*
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
 
-/*  Jasmine is a behavior-driven development framework for testing JavaScript code. 
- *  It does not depend on any other JavaScript frameworks. It does not require a DOM. 
- *  And it has a clean, obvious syntax so that you can easily write tests. 
- *      Source: jasmine.github.io
- */
+ //We're placing all of our tests within the $() function,
+ //to ensure they don't run until the DOM is ready.
+ 
+//RSS FEEDS/allFeeds
+    //This suite is all about the RSS feeds definitions, 
+    //referenced as the allFeeds variable in our application.
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
-
-
-
-
-/* RSS Feeds/allFeeds
- *  This suite is all about the RSS feeds definitions, 
- *  referenced as the allFeeds variable in our application.
- */
 $(function() {
 
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        // Udacity Written - Test allFeeds variable to ensure it is 
+        // defined & not empty.
         it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -44,11 +31,9 @@ $(function() {
             for(let feed of allFeeds) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
-            }
-            
-        })
-
-    
+            } 
+        });
+        
          //NAME LOOP
         it('Name defined', () => {
             for(let feed of allFeeds) {
@@ -58,8 +43,8 @@ $(function() {
         });    
     });
 
-//MENU
-
+//THE MENU
+    
     /* TODO: Write a new test suite named "The menu" */
 
         //DEFAULT HIDDEN
